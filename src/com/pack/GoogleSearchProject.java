3f0fd8285a8bb27074c984a75f0ff20f5b1ca930
Dasepalle <abhishekh.dasepalle@accenture.com>
2020-10-21 17:04:15 +0530

    testing

---
+++ src/com/pack/GoogleSearchProject.java
@@ -0,0 +1,16 @@
+package com.pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleSearchProject {
+public static void main(String...args) {
+		
+		WebDriver driver  = new FirefoxDriver();
+		driver.navigate().to("http://google.com");
+		String appTitle = driver.getTitle();
+		System.out.println("Application title is :: "+appTitle);
+		driver.quit();
+	}
+
+}
